0889de637073e9f263081940d63c553b2c56d9d8
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-19 13:20:47 +0000

    basic operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,22 @@
+let value = 10
+let negValue = -value
+console.log(negValue) //o/p is -10
+
+//Arithmetic 
+
+console.log(2+2);
+console.log(3-2);
+console.log(6*5);
+console.log(3**4);// it means 3 to the power 4
+console.log(2/3);
+console.log(2%3);
+
+
+// addition of string
+
+let str1="hii"
+let str2=" Aditya"
+
+let str3= str1 + str2
+console.log(str3);
+
