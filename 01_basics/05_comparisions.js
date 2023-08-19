832ee94543188dcc0e94a24c7f39c9e36167af25
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-19 14:06:06 +0000

    basic and tricky comparisions

---
+++ 01_basics/05_comparisions.js
@@ -0,0 +1,41 @@
+
+//BASIC COMPARISIONS
+
+console.log(5>3)
+console.log(5>=3)
+console.log(5<3)
+console.log(5==3)
+console.log(5!=3)
+
+
+//TRICKY COMPARISIONS
+
+console.log("5">3) //o/p is true
+
+
+
+console.log(null>0) //o/p is false
+console.log(null>=0) //o/p is true
+console.log(null<0) //o/p is false
+console.log(null==0) //o/p is false
+
+
+//  comparision check(< , > , <= , >=) and equality check(==) works differently , 
+// comparision convert null to a number , treating as 0.
+// thats why null>=0 is true while null>0 is false 
+
+
+console.log(undefined>0) //o/p is false
+
+console.log(undefined<0) //o/p is false
+
+console.log(undefined==0) //o/p is false
+
+
+
+// STRICT CHECK(===) , it even checks datatype too
+
+console.log("3"===2) //o/p is false
+
+
+// so in a nutshell we should avoid these types of comparisions
