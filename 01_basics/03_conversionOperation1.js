bebc33847a2faf2044908bfcc9da9f629d75c8dd
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-19 12:46:46 +0000

    about NaN

---
+++ 01_basics/03_conversionOperation1.js
@@ -0,0 +1,9 @@
+let score = "33abc"
+
+console.log(typeof(score))
+
+let valueInNumber = Number(score) //remember first letter "N" is caoital
+
+console.log(typeof valueInNumber) //here also o/p will be number
+
+console.log(valueInNumber) // its o/p is NaN, this is special type of data , named as not a number
