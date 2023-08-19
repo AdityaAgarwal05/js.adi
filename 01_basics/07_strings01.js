32e5af2b05f1d68e40f1ac33ffccfe200d985350
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-19 21:14:51 +0000

    some string methods

--- 01_basics/07_strings01.js
+++
@@ -1,9 +0,0 @@
-// in JS  strings can be represnt in both either single quote('') , or double quote("")
-
-const name = "aditya"
-const age = 21
-console.log( name + age +  "male") //o/p is aditya21male
-
-//this is outdated syntax , in moder4n days it is replaced by backticks(` `) representation
-
-console.log(`hi my name is ${name} and my age is ${age}`)
---
+++ 01_basics/07_strings2.js
@@ -0,0 +1,7 @@
+const gameName = new String(`aditya`)  // one of the way to declare string
+console.log(gameName[0]);//o/p is "a"
+console.log(gameName.__proto__); // syntax to use prototuypes of string , here o/p is an empty object {}
+console.log(gameName.length) // o/p is 6
+console.log(gameName.toUpperCase() ) // o/p is ADITYA
+console.log(gameName.charAt(4) ) //o/p is y
+console.log(gameName.indexOf('t') ) //o/p is 3
