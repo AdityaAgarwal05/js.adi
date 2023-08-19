c856025efff3f9fc42066580d5051287462282fe
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-19 13:09:34 +0000

    multiple conversions

---
+++ 01_basics/03_conversionOperation2.js
@@ -0,0 +1,58 @@
+//for null
+
+let score1 = null
+
+console.log(typeof(score1)) //o/p is object
+
+let valueInNumber1 = Number(score1) //remember first letter "N" is capital
+
+console.log(typeof valueInNumber1) //o/p is number
+
+
+//for undefined-
+
+
+let score2 = undefined
+
+console.log(typeof(score2)) //o/p is "undefined"
+
+let valueInNumber2 = Number(score2) //remember first letter "N" is caoital
+
+console.log(typeof valueInNumber2) //o/p is number
+
+  
+//for boolean-
+
+
+let score3 = true
+
+console.log(typeof(score3)) //o/p is boolean
+
+let valueInNumber3 = Number(score3) //remember first letter "N" is caoital
+
+console.log(typeof valueInNumber3) //o/p is number
+
+
+//for string
+
+let score4 = "Aditya"
+
+console.log(typeof(score4)) //o/p is string
+
+let valueInNumber4 = Number(score4) //remember first letter "N" is caoital
+
+console.log(typeof valueInNumber4) //o/p is number
+
+
+//to convert in boolean
+
+
+let IsLoggedIn =1
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn) //REMEMBER ist letter capital
+console.log(booleanIsLoggedIn) //o/p is "true"
+
+let someNumber = 50
+let stringNumber = String(someNumber)
+console.log(stringNumber)  //o/p is 50
+console.log(typeof(stringNumber)) //o/p is string
