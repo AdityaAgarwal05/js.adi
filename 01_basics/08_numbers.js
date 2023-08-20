413f1b4d8eca1237fb7ea79975666cc716e51989
Aditya Agarwal <122960251+AdityaAgarwal05@users.noreply.github.com>
2023-08-20 14:35:13 +0000

    methods of numbers

--- 01_basics/08_numbers.js
+++ 01_basics/08_numbers.js
@@ -1,21 +1,21 @@
 const score = 400
-console.log(score) //JS automatically identify it as a number
+console.log(score) //JS automatically identify it as a number , o/p  is 400
 
 const balance = new Number(100) //here explecitelly , we forced to treat or make it as a number
-console.log(balance);
+console.log(balance); // o/p is [Number: 100]
 
 // SOME METHODS OF NUMBERS
 
 
 //converting number into string and then measured the length
-console.log(balance.toString().length);
+console.log(balance.toString().length); // o/p is 3
 //to convert a number in more precision value
-console.log(balance.toFixed(2));
+console.log(balance.toFixed(2)); // o/p is 100.00
 
 const otherNumber = 123.8996
-console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(3)); // o/p is 124
 
 
 const hundreds = 1000000
-console.log(hundreds.toLocaleString())
-console.log(hundreds.toLocaleString('en-IN'))
+console.log(hundreds.toLocaleString()) // o/p is 1,000,000
+console.log(hundreds.toLocaleString('en-IN')) // o/p is 10,00,000
